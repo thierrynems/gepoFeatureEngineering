4d0fc3b4a2be245e968070ad5da1557b5d250515
KONGNE N. Thierry <thierrynemsk@gmail.com>
2021-02-26 16:40:28 +0100

    Update scriptFeatureBySeq.R
    remove argument to ReadFASTA

--- scriptFeatureBySeq.R
+++ scriptFeatureBySeq.R
@@ -8,12 +8,13 @@ geneFile <- args[1]
 
 proteinFile <- args[2]
 
-peptide_list_essential=readFASTA(proteinFile,sep="")
-coding_list_essential=readFASTA(geneFile,sep="")
+peptide_list_essential=readFASTA(proteinFile)
+coding_list_essential=readFASTA(geneFile)
 
 
 
 peptide_list_essential <- peptide_list_essential[(sapply(peptide_list_essential, protcheck))]
+print(peptide_list_essential)
 #peptide_list_nessential <- peptide_list_nessential[(sapply(peptide_list_nessential, protcheck))]
 #peptide_list_cessential <- peptide_list_cessential[(sapply(peptide_list_cessential, protcheck))]
 
